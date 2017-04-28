7f60380ee590705e050abb707441e30d6d7ded41
Andrew Hastings <droo.hastings@gmail.com>
2017-04-28 09:23:56 -0500

    initial file srtructure

---
+++ server.js
@@ -0,0 +1,29 @@
+//requires
+var express = require ('express');
+var app = express();
+var pg = require ('pg');
+var path = require('path');
+var bodyParser = require ('body-parser');
+
+//port
+var port = 8888;
+
+var config = {
+  database: 'pets',
+  host: 'localhost',
+  port: 5432,
+  max: 12
+
+};// end config
+
+
+// uses
+app.use(express.static('public/views'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//create server
+app.listen (port, function(){
+  console.log('server up on:', port);
+
+});// end app.listen
